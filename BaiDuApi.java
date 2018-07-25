4c671c383793685ebbf59e8e52b8be08c42bd634
Muggle <1992@1992.cool>
2018-07-25 17:15:34 +0800

    Update BaiDuApi.java

--- BaiDuApi.java
+++ BaiDuApi.java
@@ -1,6 +1,7 @@
 package onemuggle;
 
 import java.net.URLEncoder;
+import java.util.Base64;
 
 public class BaiDuApi {
 
@@ -9,8 +10,8 @@ public class BaiDuApi {
         // 本地图片路径
         String filePath = "C:\\Users\\Neuer\\Desktop\\333.png";
         try {
-            byte[] imgData = OcrFileUtil.readFileByBytes(filePath);
-            String imgStr = OcrBase64Util.encode(imgData);
+            byte[] imgData = FileUtil.readFileByBytes(filePath);
+            String imgStr = Base64.getEncoder().encodeToString(imgData);
             String params = URLEncoder.encode("image", "UTF-8") + "=" + URLEncoder.encode(imgStr, "UTF-8");
             String accessToken = AuthService.getAuth();
             String result = BaiDuHttpUtil.post(apihost, accessToken, params);
